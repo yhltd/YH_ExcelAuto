cd8be669966cc1e67e7f9e2d68b088763f0c9cce
Lewishui <caoyuanlang901029@126.com>
2019-11-13 16:44:56 +0800

    up scan lewis

---
+++ YH_ScanProject/frmmain.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace YH_ScanProject
+{
+    partial class frmmain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.importButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.openFileBtton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(814, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(814, 420);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(806, 394);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "单件作业";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.closeButton);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.importButton);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.pathTextBox);
+            this.tabPage2.Controls.Add(this.openFileBtton);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(806, 394);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "批量作业";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip2.Location = new System.Drawing.Point(0, 445);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip2.Size = new System.Drawing.Size(814, 25);
+            this.toolStrip2.TabIndex = 2;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 11F);
+            this.label4.Location = new System.Drawing.Point(138, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "选择图片：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(226, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 21);
+            this.textBox1.TabIndex = 33;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(556, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(36, 27);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox2.Font = new System.Drawing.Font("宋体", 11F);
+            this.textBox2.Location = new System.Drawing.Point(3, 108);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(800, 280);
+            this.textBox2.TabIndex = 35;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "识别结果：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 11F);
+            this.label2.Location = new System.Drawing.Point(165, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 15);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "输出模板路径：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(279, 107);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(324, 21);
+            this.textBox3.TabIndex = 40;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(611, 104);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(36, 27);
+            this.button2.TabIndex = 39;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(277, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 38;
+            // 
+            // closeButton
+            // 
+            this.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.closeButton.Location = new System.Drawing.Point(497, 219);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(106, 32);
+            this.closeButton.TabIndex = 34;
+            this.closeButton.Text = "关闭";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("MS PGothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(144, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(534, 27);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "Excel自动化工具";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // importButton
+            // 
+            this.importButton.Location = new System.Drawing.Point(279, 219);
+            this.importButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(106, 32);
+            this.importButton.TabIndex = 33;
+            this.importButton.Text = "开始";
+            this.importButton.UseVisualStyleBackColor = true;
+            this.importButton.Click += new System.EventHandler(this.importButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 11F);
+            this.label6.Location = new System.Drawing.Point(105, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(172, 15);
+            this.label6.TabIndex = 36;
+            this.label6.Text = "数据源路径（文件夹）：";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(279, 68);
+            this.pathTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(324, 21);
+            this.pathTextBox.TabIndex = 35;
+            // 
+            // openFileBtton
+            // 
+            this.openFileBtton.Location = new System.Drawing.Point(611, 65);
+            this.openFileBtton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.openFileBtton.Name = "openFileBtton";
+            this.openFileBtton.Size = new System.Drawing.Size(36, 27);
+            this.openFileBtton.TabIndex = 32;
+            this.openFileBtton.Text = "...";
+            this.openFileBtton.UseVisualStyleBackColor = true;
+            this.openFileBtton.Click += new System.EventHandler(this.openFileBtton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 11F);
+            this.label7.Location = new System.Drawing.Point(653, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 15);
+            this.label7.TabIndex = 42;
+            // 
+            // frmmain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 470);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.toolStrip2);
+            this.Name = "frmmain";
+            this.Text = "云合未来计算机技术有限公司";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button importButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button openFileBtton;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ YH_ScanProject/frmmain.cs
@@ -0,0 +1,210 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YH_ScanProject
+{
+    public partial class frmmain : Form
+    {
+        private string Copyfile = "";
+        public string path;
+        private List<string> Alist = new List<string>();
+        string fullname; //文件路径+文件名，用于保存
+        public frmmain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+            OpenFileDialog1.Filter = "bmp,jpg,gif,png,tiff,icon|*.bmp;*.jpg;*.gif;*.png;*.tiff;*.icon";
+            OpenFileDialog1.Title = "选择图片";
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fullname = OpenFileDialog1.FileName.ToString();
+            }
+
+
+            string ax = scanstep(fullname);
+
+            this.textBox2.Text = ax.ToString();
+
+
+        }
+
+        private static string scanstep(string fullname)
+        {
+            //string path = AppDomain.CurrentDomain.BaseDirectory + "1.jpg";
+            //path = AppDomain.CurrentDomain.BaseDirectory + "Capture321.JPG";
+
+            var ApiKey = "e375aac2fd624863b631ec5e45c81bdb";
+            var SecretKey = "ecee0983771f451ab86ef6fea63b4847";
+            var tuPian = fullname;
+
+            var client = new Baidu.Aip.Ocr.Ocr(ApiKey, SecretKey);
+            var image = File.ReadAllBytes(tuPian);
+
+            // 通用文字识别
+            var result = client.GeneralBasic(image, null);
+
+            string ax = "";
+
+            JsonTextReader reader = new JsonTextReader(new StringReader(result.ToString()));
+            while (reader.Read())
+            {
+                if (reader.Value != null && reader.Value.ToString() != "words" && reader.Value.ToString() != "words_result" && reader.Value.ToString() != "words_result_num")
+                {
+                    //Console.WriteLine("Token: {0}, Value: {1}", reader.TokenType, reader.Value);
+                    ax += "\r\n" + reader.Value;
+                }
+                //else
+                //    Console.WriteLine("Token: {0}", reader.TokenType);
+            }
+            return ax;
+        }
+
+        private void openFileBtton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "请选择所在文件夹";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+                path = dialog.SelectedPath;
+                pathTextBox.Text = dialog.SelectedPath;
+
+
+            }
+            else
+                return;
+
+            Alist = new List<string>();
+
+            Alist = GetBy_CategoryReportFileName(path);
+
+            label7.Text = "已选中：" + Alist.Count();
+
+        }
+        public List<string> GetBy_CategoryReportFileName(string dirPath)
+        {
+
+            List<string> FileNameList = new List<string>();
+            ArrayList list = new ArrayList();
+
+            if (Directory.Exists(dirPath))
+            {
+                list.AddRange(Directory.GetFiles(dirPath));
+            }
+            if (list.Count > 0)
+            {
+                foreach (object item in list)
+                {
+                    if (!item.ToString().Contains("~$"))
+                        //FileNameList.Add(item.ToString().Replace(dirPath + "\\", ""));
+                        FileNameList.Add(item.ToString());
+                }
+            }
+
+            return FileNameList;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog tbox = new OpenFileDialog();
+            tbox.Multiselect = false;
+            tbox.Filter = "Excel Files(*.xls,*.xlsx,*.xlsm,*.xlsb)|*.xls;*.xlsx;*.xlsm;*.xlsb";
+            if (tbox.ShowDialog() == DialogResult.OK)
+            {
+                Copyfile = tbox.FileName;
+                textBox3.Text = Copyfile;
+
+            }
+        }
+
+        private void importButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Alist.Count; i++)
+            {
+                //GetKEYnfo(path + "\\" + Alist[i]);
+
+                string ax = scanstep(Alist[i]);
+            }
+
+           
+
+            //downcsv(dataGridView);
+
+        }
+
+        public void downcsv(DataGridView dataGridView)
+        {
+            if (dataGridView.Rows.Count == 0)
+            {
+                MessageBox.Show("Sorry , No Data Output !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = ".csv";
+            saveFileDialog.Filter = "csv|*.csv";
+            string strFileName = "  下载信息" + "_" + DateTime.Now.ToString("yyyyMMddHHmmss");
+            saveFileDialog.FileName = strFileName;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                strFileName = saveFileDialog.FileName.ToString();
+            }
+            else
+            {
+                return;
+            }
+            FileStream fa = new FileStream(strFileName, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fa, Encoding.Unicode);
+            string delimiter = "\t";
+            string strHeader = "";
+            for (int i = 0; i < dataGridView.Columns.Count; i++)
+            {
+                strHeader += dataGridView.Columns[i].HeaderText + delimiter;
+            }
+            sw.WriteLine(strHeader);
+
+            //output rows data
+            for (int j = 0; j < dataGridView.Rows.Count; j++)
+            {
+                string strRowValue = "";
+
+                for (int k = 0; k < dataGridView.Columns.Count; k++)
+                {
+                    if (dataGridView.Rows[j].Cells[k].Value != null)
+                    {
+                        strRowValue += dataGridView.Rows[j].Cells[k].Value.ToString().Replace("\r\n", " ").Replace("\n", "") + delimiter;
+
+
+                    }
+                    else
+                    {
+                        strRowValue += dataGridView.Rows[j].Cells[k].Value + delimiter;
+                    }
+                }
+                sw.WriteLine(strRowValue);
+            }
+            sw.Close();
+            fa.Close();
+            MessageBox.Show("下载完成 ！", "System", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+    }
+}
